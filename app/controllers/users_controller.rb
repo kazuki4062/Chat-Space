6272ed6857c74659fc60e9a445b02212a4ede64b
kazukisato <sato87163868@gmail.com>
2019-08-20 11:58:36 +0900

    aaa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?) and id != ?', "#{params[:name]}%", current_user)
-    respond_to do |format|
-      format.html
-      format.json
+      @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+      respond_to do |format|
+        format.html
+        format.json
+      end
     end
-  end
 
   def edit
   end
